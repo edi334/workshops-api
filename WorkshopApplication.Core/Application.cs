ec37ac5012257f75c32c6be599f8200d10973e85
Edi Cziszter <eduard.cziszter@student.upt.ro>
2022-03-17 11:46:14 +0200

    added base entities

---
+++ WorkshopApplication.Core/Application.cs
@@ -0,0 +1,11 @@
+namespace WorkshopApplication.Core;
+
+public class Application
+{
+    public Guid Id { get; set; }
+    public string Gender { get; set; }
+    public string Country { get; set; }
+    public string University { get; set; }
+    public string FieldOfStudy { get; set; }
+    public string Reason { get; set; }
+}
---
+++ WorkshopApplication.Core/Workshop.cs
@@ -0,0 +1,9 @@
+namespace WorkshopApplication.Core;
+
+public class Workshop
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+}
